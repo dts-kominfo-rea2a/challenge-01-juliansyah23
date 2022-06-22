3eab6f035467d53660d6c2f524178836e9a32fb9
rival <rivaladrianjuliansyah@gmail.com>
2022-06-22 09:02:37 +0700

    Struktur Data

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+
+const firstUser = {
+    name : "Rival Adrian Juliansyah",
+    gender : "Laki-Laki",
+    age : 24,
+    email : "rival@juliansyah.my.id",
+    favoriteColor : ["Hitam", "Putih", "Abu-Abu", "Biru"],
+    isHavePet : true,
+    education : [
+            {name : "SD", city : "Bandung", graduation : 2009},
+            {name : "SMP", city : "Bandung", graduation : 2012},
+            {name : "SMA", city : "Bandung", graduation : 2015},
+        ],
+    favoriteRestaurant : {bakso : "Semar", minuman : "Mixue", kebab : "Kebab Bosman"},
+};
+const secondUser = {
+    name : "Wendy",
+    gender : "Male",
+    age : 23,
+    email : "wendy@dingdong.com",
+    favoriteColor : ["Blue", "Black", "Gray"],
+    isHavePet : false,
+    education : [
+        {name : "SD 02", city : "Jakarta", graduation : 2010},
+        {name : "SMP 03", city : "Bogor", graduation : 2013},
+        {name : "SMA 01", city : "Surabaya", graduation : 2016},
+        {name : "Universitas Maju", city : "Tangerang"},
+    ],
+    favoriteRestaurant : {tempura : "Yusukekun", katsu : "Rabenshi", geprek : "KFC"},
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
+users.push(firstUser,secondUser);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
